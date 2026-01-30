74126006370ca48a264fd46d436d9e94cfbad429
Sowmiya S <sowmiyasenthil426@gmail.com>
2026-01-30 21:11:41 +0530

    isEven.java

--- isEven.java
+++ isEven.java
@@ -4,11 +4,11 @@ public class isEven{
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number:");
         int num = sc.nextInt();
-        if((num / 2) * 2 == num){ // We can use another method (num % 2 == 0)
-            System.out.println("It is Even Number");
+        if((num / 2) * 2 == num){ // We can also use another method (num % 2 == 0)
+            System.out.println("Even Number");
         }
         else{
-            System.out.println("It is Not a Even number");
+            System.out.println("Not a Even number");
         }
     }
 }
